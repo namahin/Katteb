d281ac712b488684177ddd63e4835af8c1fe6a9b
Noore Alam Mahin <mahin0w1@gmail.com>
2022-02-21 23:14:10 +0600

    fix counter

--- js/app.js
+++ js/app.js
@@ -2,14 +2,37 @@
 new WOW().init();
 
 // number counter
-// $('.count').each(function () {
-//     $(this).prop('Counter',0.0).animate({
-//         Counter: $(this).text()
-//     }, {
-//         duration: 4000,
-//         easing: 'swing',
-//         step: function (now) {
-//             $(this).text(Math.ceil(now));
-//         }
-//     });
-// });
+var a = 0;
+$(window).scroll(function () {
+    var oTop = $("#counter-box").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $(".counter").each(function () {
+            var $this = $(this),
+                countTo = $this.attr("data-number");
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+                    duration: 2000,
+                    easing: "linear",
+                    step: function () {
+                        //$this.text(Math.ceil(this.countNum));
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                    },
+                    complete: function () {
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                        //alert('finished');
+                    }
+                }
+            );
+        });
+        a = 1;
+    }
+});
