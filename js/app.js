f0c9fa8e1c85198734cffe2d6b22da2fa5ad0285
Noore Alam Mahin <mahin0w1@gmail.com>
2022-02-21 16:43:46 +0600

    fix header-footer

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 // active wow js
 new WOW().init();
+
+// number counter
+$('.count').each(function () {
+    $(this).prop('Counter',0.0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
