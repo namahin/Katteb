0e63b2af255e7d736d5d9af8b04cf1ff4eea1a5d
Noore Alam Mahin <mahin0w1@gmail.com>
2022-02-21 17:44:32 +0600

    fix null css

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 new WOW().init();
 
 // number counter
-$('.count').each(function () {
-    $(this).prop('Counter',0.0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+// $('.count').each(function () {
+//     $(this).prop('Counter',0.0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 4000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
